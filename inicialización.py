d7b3ace1bdfea91e5a04f94a73d025accaabdb5b
Sanchezj <sanchez.jmm@googlemail.com>
2019-04-18 19:28:22 +0200

    Annadida la selección por torneo (testeo pendiente)
    Modificada la función SUS (verificación y testeo pendientes --> como generar una población mayor que el número de padres? (pues este puede ser menor que el tammanno de la población al haber cambiado el pool al eliminar de él los individuos inválidos)

--- inicialización.py
+++ inicialización.py
@@ -34,25 +34,25 @@ def inicia_rhh(_tamano_poblacion, min_long_fenotipo, max_long_fenotipo):
     for longitud in longitudes_fenotipos:
         for i in range(num_vueltas):
             # Genera individuo con el métod "Grow"
-            indiv = genera_indiv_aleat(longitud)
-            poblacion.append(indiv)
+            genotipo = genera_indiv_aleat(longitud)
+            poblacion.append(Individuo(genotipo))
 
             # Genera individuo con el método "Full"
-            indiv = genera_indiv_full(longitud)
-            poblacion.append(indiv)
+            genotipo = genera_indiv_full(longitud)
+            poblacion.append(Individuo(genotipo))
 
-    if resto: # si aún no se ha llegado a completar la población deseada
+    if resto:  # si aún no se ha llegado a completar la población deseada
         longitudes_fenotipos = list(longitudes_fenotipos)
         random.shuffle(longitudes_fenotipos)
 
     for i in range(resto):
         longitud = longitudes_fenotipos.pop()
 
-        indiv = genera_indiv_aleat(longitud)
-        poblacion.append(indiv)
+        genotipo = genera_indiv_aleat(longitud)
+        poblacion.append(Individuo(genotipo))
 
-        indiv = genera_indiv_full(longitud)
-        poblacion.append(indiv)
+        genotipo = genera_indiv_full(longitud)
+        poblacion.append(Individuo(genotipo))
 
     return poblacion
 
@@ -80,3 +80,9 @@ def genera_indiv_aleat(longitud_fenotipo, codones_kernel = 15):
 
 test = genera_indiv_full(5)
 print(test)
+print("\n\n\n")
+
+test2 = inicia_rhh(8, 2, 5)
+
+for el in test2:
+    print("\n\n", el)
